dbc388be8f2b44b70d7758fc4b5f03001cb3f4da
Svetlana Vasilyuk <lanarusufa@gmail.com>
2020-02-15 17:34:11 +0300

    Добавлена задача 1.1. Нахождение минимального из двух чисел

---
+++ src/FindMinNumber/Main.java
@@ -0,0 +1,25 @@
+package FindMinNumber;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static double findMin(double a, double b){
+        if (a<b) {
+            return a;
+        }
+        else {
+           return b;
+        }
+    }
+
+    public static void main(String[] args) {
+        double a, b;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        a=scanner.nextDouble();
+        System.out.println("Введите второе число: ");
+        b=scanner.nextDouble();
+        System.out.println("Минимальным из чисел является число " + findMin(a, b));
+    }
+}
